7195834c8d763ce5bfc15c5c665555e86bb27e8a
Robin Wragg <robin.wragg@student.nua.ac.uk>
2019-12-12 13:40:31 +0000

    Better windows shader compilation

--- code/platforms/windows/LightAndShadow/compile_shaders.bat
+++ code/platforms/windows/LightAndShadow/compile_shaders.bat
@@ -1,8 +1,6 @@
-IF EXIST "build/basic_vert.spv" (DEL "build/basic_vert.spv")
-IF EXIST "build/basic_frag.spv" (DEL "build/basic_frag.spv")
 
-"VulkanSDK 1.1.121.2/Bin/glslc.exe" VulkanParticleSystem/basic.vert -o build/basic_vert.spv
+"../VulkanSDK 1.1.121.2/Bin/glslc.exe" ../../../glsl/basic.vert -o ../x64/Debug/assets/basic_vert.spv
 IF %ERRORLEVEL% NEQ 0 (pause)
 
-"VulkanSDK 1.1.121.2/Bin/glslc.exe" VulkanParticleSystem/basic.frag -o build/basic_frag.spv
+"../VulkanSDK 1.1.121.2/Bin/glslc.exe" ../../../glsl/basic.frag -o ../x64/Debug/assets/basic_frag.spv
 IF %ERRORLEVEL% NEQ 0 (pause)
