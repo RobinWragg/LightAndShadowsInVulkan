8855fc996e7611f428a11fda5ebe53aade5b60b5
Robin Wragg <robin.wragg@me.com>
2019-12-15 17:40:21 +0000

    Add comment about disabling warnings

--- code/cpp/main.h
+++ code/cpp/main.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Disable warnings for conversions from double to float. Doubles aren't used in this project; disabling these warnings just means I don't have to put an "f" after every float literal.
 #pragma warning( disable : 4244 )
 #pragma warning( disable : 4305 )
 
