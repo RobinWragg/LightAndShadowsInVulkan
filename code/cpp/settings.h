459a261a64409f8f3275193200fbc61198e73666
Robin Wragg <robin.wragg@me.com>
2020-05-19 18:32:11 +0100

    Update settings

--- code/cpp/settings.h
+++ code/cpp/settings.h
@@ -1,25 +1,25 @@
 #pragma once
 
-#define SHADOWMAP_RESOLUTION 4096
+#define SHADOWMAP_RESOLUTION 2048
 #define MAX_LIGHT_SUBSOURCE_COUNT 14
 #define MAX_SHADOW_ANTI_ALIAS_SIZE 10
 #define MSAA_SETTING VK_SAMPLE_COUNT_8_BIT
 
 struct Settings {
-  int subsourceCount = 1;
+  int subsourceCount = 8;
   
   enum {
     RING,
     SPIRAL
   } subsourceArrangement = SPIRAL;
   
-  bool animateLightPos = false;
+  bool animateLightPos = true;
   bool renderTextures = true;
   bool renderNormalMaps = true;
   float ambReflection = 0.2;
   
-  float sourceRadius = 0.1;
-  int shadowAntiAliasSize = 0;
+  float sourceRadius = 0.4;
+  int shadowAntiAliasSize = 2;
 };
 
 extern Settings settings;
